fef1a77ee3a004043cb849a7f9dbf01c787e6b1b
Khoi Ngo <khoi.ngo@logigear.com>
2017-07-31 11:19:25 +0700

    aaa

---
+++ projectA/src/khoi/classB.py
@@ -0,0 +1,19 @@
+'''
+Created on Jul 27, 2017
+
+@author: khoi.ngo
+'''
+
+class B(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+    @staticmethod
+    def printB():
+        print("123 B class")
---
+++ projectA/src/setup.py
@@ -0,0 +1,16 @@
+'''
+Created on Jul 28, 2017
+
+@author: khoi.ngo
+'''
+from setuptools import setup
+
+setup(name='abc',
+      version='0.1',
+      description='Test uploada project',
+      url='',
+      author='khoi.ngo',
+      author_email='quainhan100@gmail.com',
+      license='MIT',
+      packages=['khoi'],
+      zip_safe=False)
